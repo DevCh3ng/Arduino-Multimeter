a04322b7363397588589a7eafbbf88f0b9e6c7ca
Tch3ng <devinjotedy89@gmail.com>
2025-06-03 22:10:24 +0800

    new version

--- ard_dmult.ino
+++ ard_dmult.ino
@@ -1,54 +1,43 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-const byte ANALOG_IN_PIN    = A0;
-const byte SENSOR_PIN       = A1;
-const byte CONTINUITY_PIN   = A2;
-const byte BUTTON_PIN       = 3;
-const byte SPEAKER_PIN      = 4;
-const byte BACKLIGHT_PIN    = 13;
+const byte VOLT_PIN = A0;
+const byte RES_PIN = A1;
+const byte CONT_PIN = A2;
+const byte AMMETER_PIN = A3;
 
-const float R1_VOLTAGE_DIVIDER = 330.0;
-const float R2_VOLTAGE_DIVIDER = 220.0;
-const float REFERENCE_VOLTAGE  = 5.0;
-const float R_REF_RESISTANCE   = 10000.0;
-const float ADC_MAX_VALUE      = 1023.0;
+const byte BUTTON_PIN = 3;
+const byte SPEAKER_PIN = 4;
+const byte BACKLIGHT_PIN = 13;
 
-const int CONTINUITY_THRESHOLD = 1010; // Adjust based on your circuit (LOW value typically means continuity)
-const unsigned int SPEAKER_FREQ = 1000;
-const int CONTINUITY_READ_COUNT = 5;
-const int CONTINUITY_READ_DELAY = 2;
+const float R1V = 330.0;
+const float R2V = 220.0;
+const float Vref = 5.0;
+const float Rref = 10000.0;
 
-const unsigned long DEBOUNCE_DELAY = 50;
+const int Cont_thres = 1010; // cont. sens 
+const float acs712_sens = 0.185;
+const float acs712_off = 2.5;
+const float Iref = 5.0;
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-const byte LCD_ADDR = 0x27;
-const byte LCD_COLS = 16;
-const byte LCD_ROWS = 2;
-LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
-
-enum Mode {
-  MODE_MENU,
-  MODE_VOLTAGE,
-  MODE_RESISTANCE,
-  MODE_CONTINUITY,
-  NUM_MODES
-};
-Mode currentMode = MODE_MENU;
-Mode previousMode = MODE_MENU;
-
+int currentMode = 0;
+int previousMode = 0;
 int currentButtonState = HIGH;
 int lastButtonState = HIGH;
+
 unsigned long lastDebounceTime = 0;
 
-void displayMenu();
+void displayMenu(); // foward decl
 void measureVoltage();
 void measureResistance();
 void checkContinuity();
-int readContinuity();
+void measureCurrent();
 void updateMode();
 
 void setup() {
-  Serial.begin(9600);
+  // if you wish to debug uncomment this
+  // Serial.begin(9600); 
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(SPEAKER_PIN, OUTPUT);
@@ -57,7 +46,7 @@ void setup() {
   noTone(SPEAKER_PIN);
   digitalWrite(BACKLIGHT_PIN, HIGH);
 
-  lcd.begin(LCD_COLS, LCD_ROWS);
+  lcd.begin(16, 2);
   lcd.init();
   lcd.backlight();
   lcd.clear();
@@ -80,23 +69,25 @@ void loop() {
   }
 
   switch (currentMode) {
-    case MODE_MENU:
-      displayMenu();
+    case 0:
+        displayMenu();
       break;
-    case MODE_VOLTAGE:
+    case 1:
       measureVoltage();
       break;
-    case MODE_RESISTANCE:
+    case 2:
       measureResistance();
       break;
-    case MODE_CONTINUITY:
+    case 3:
       checkContinuity();
+      break;
+    case 4:
+      measureCurrent();
       break;
   }
 
   delay(150);
 }
-
 
 void updateMode() {
   int reading = digitalRead(BUTTON_PIN);
@@ -105,11 +96,11 @@ void updateMode() {
     lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+  if ((millis() - lastDebounceTime) > 50) {
     if (reading != currentButtonState) {
       currentButtonState = reading;
       if (currentButtonState == LOW) {
-        currentMode = (Mode)((currentMode + 1) % NUM_MODES);
+        currentMode = (int)((currentMode + 1) % 5);
       }
     }
   }
@@ -120,59 +111,65 @@ void displayMenu() {
   lcd.setCursor(0, 0);
   lcd.print(F("Press button"));
   lcd.setCursor(0, 1);
-  lcd.print(F("for V, R, Cont"));
+  lcd.print(F("V, R, Cont, A"));
 }
 
 void measureVoltage() {
-  int adc_value = analogRead(ANALOG_IN_PIN);
-  float adc_voltage = (adc_value / ADC_MAX_VALUE) * REFERENCE_VOLTAGE;
-  float in_voltage = adc_voltage * (R1_VOLTAGE_DIVIDER + R2_VOLTAGE_DIVIDER) / R2_VOLTAGE_DIVIDER;
+  int adc_value = analogRead(VOLT_PIN);
+  float adc_voltage = (adc_value / 1023) * Vref;
+  float in_voltage = adc_voltage * (R1V + R2V) / R2V;
 
-  if (in_voltage < 0.01) {
+  if (in_voltage < 0.03) {
       in_voltage = 0.0;
   }
 
-  Serial.print(in_voltage, 2);
-  Serial.println(" V");
-
-  lcd.setCursor(0, 0);
-  lcd.print(F("Voltage:"));
+  // Serial.print(in_voltage, 2);
+  // Serial.println(F(" V"));
+  lcd.setCursor(0,0);
+  lcd.print(F("Voltage"));
   lcd.setCursor(0, 1);
   lcd.print(in_voltage, 2);
   lcd.print(F(" V       "));
 }
 
 void measureResistance() {
-  int sensorValue = analogRead(SENSOR_PIN);
-  float Vout = (sensorValue / ADC_MAX_VALUE) * REFERENCE_VOLTAGE;
+  int sensorValue = analogRead(RES_PIN);
+  float Vout = (sensorValue / 1023) * Vref;
   float R = 0.0;
 
   if (sensorValue <= 1) {
     R = 0.0;
-  } else if (sensorValue >= (int)(ADC_MAX_VALUE - 1) ) {
-    R = -1.0; // Flag for OL
-  } else {
-     R = R_REF_RESISTANCE * (1.0 / ((REFERENCE_VOLTAGE / Vout) - 1.0));
-     if (R < 0) R = -1.0;
+  } else if (sensorValue >= (1023 - 1) ) {
+    R = -1.0;
+  } else if (Vout >= Vref) {
+    R = -1.0;
+  }
+  else {
+     if ((Vref / Vout) <= 1.0) {
+        R = -1.0;
+     } else {
+        R = Rref * (1.0 / ((Vref / Vout) - 1.0));
+     }
+     if (R < 0 && R != -1.0) R = -1.0;
   }
 
-  Serial.print("R: ");
-  if (R < 0) {
-      Serial.println("OL");
+  /*Serial.print(F("R: "));
+  if (R == -1.0) {
+      Serial.println(F("OL"));
   } else {
       Serial.print(R, 1);
-      Serial.println(" ohm");
-  }
+      Serial.println(F(" ohm"));
+  }*/
 
   lcd.setCursor(0, 0);
   lcd.print(F("Resistance:"));
   lcd.setCursor(0, 1);
-  if (R < 0) {
+  if (R == -1.0) {
     lcd.print(F("OL         "));
-  } else if (R > 1000000) {
+  } else if (R > 999999.9) {
     lcd.print(R / 1000000.0, 2);
     lcd.print(F(" MOhm   "));
-  } else if (R > 1000) {
+  } else if (R > 999.9) {
     lcd.print(R / 1000.0, 2);
     lcd.print(F(" kOhm   "));
   } else {
@@ -182,17 +179,21 @@ void measureResistance() {
 }
 
 void checkContinuity() {
-  int sensorValue = readContinuity();
-
-  Serial.print("Continuity ADC: ");
-  Serial.println(sensorValue);
+  long sum = 0;
+  for (int i = 0; i < 5; i++) {
+    sum += analogRead(CONT_PIN);
+    delay(2);
+  }
+  int sensorValue = sum / 5;
+  // Serial.print(F("Continuity ADC: "));
+  // Serial.println(sensorValue);
 
   lcd.setCursor(0, 0);
   lcd.print(F("Continuity:"));
   lcd.setCursor(0, 1);
 
-  if (sensorValue > CONTINUITY_THRESHOLD) { // Low value assumed for continuity
-    tone(SPEAKER_PIN, SPEAKER_FREQ);
+  if (sensorValue > Cont_thres) {
+    tone(SPEAKER_PIN, 1000);
     lcd.print(F("Connected  "));
   } else {
     noTone(SPEAKER_PIN);
@@ -200,11 +201,36 @@ void checkContinuity() {
   }
 }
 
-int readContinuity() {
-  long sum = 0;
-  for (int i = 0; i < CONTINUITY_READ_COUNT; i++) {
-    sum += analogRead(CONTINUITY_PIN);
-    delay(CONTINUITY_READ_DELAY);
+void measureCurrent() {
+  float adc_sum = 0;
+  for (int i = 0; i < 20; i++) {
+    adc_sum += analogRead(AMMETER_PIN);
   }
-  return sum / CONTINUITY_READ_COUNT;
+  float adc_average = adc_sum / 20;
+  adc_average = adc_average > 516.20 ? adc_average : 511.5;
+  // Serial.println(adc_average);
+  float voltage_at_pin = (float)((adc_average * 5.0) / 1023) ;
+  float I = (voltage_at_pin - 2.5)/(acs712_sens*10);
+  // Serial.println(adc_average);
+  lcd.setCursor(0, 0);
+  lcd.print(F("Current:"));
+  lcd.setCursor(0, 1);
+  
+
+  if (I < 1.0 && I != 0.0 ) {
+    lcd.print(I * 1000.0, 1);
+    lcd.print(F(" mA    "));
+  } else {
+    lcd.print(I, 2);
+    lcd.print(F(" A     "));
+  }
+  if (I >= 10.0) {
+      lcd.print(F("   "));
+  } else if (I >= 1.0) {
+      lcd.print(F("    "));
+  } else if (I * 1000.0 >= 100.0){
+      lcd.print(F("  "));
+  } else if (I * 1000.0 >= 10.0){
+      lcd.print(F("   "));
+  }
 }
